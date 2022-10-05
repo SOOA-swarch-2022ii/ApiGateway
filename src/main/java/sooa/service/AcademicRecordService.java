9cbdce905bf67652e5d5f8e38e0c3d4c572f6845
Diego Irreño <dirreno@unal.edu.co>
2022-10-04 22:00:51 -0500

    AcademicRecord branch m

--- src/main/java/sooa/service/AcademicRecordService.java
+++ src/main/java/sooa/service/AcademicRecordService.java
@@ -1,9 +1,13 @@
 package sooa.service;
 
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import sooa.domain.academic_record_ms.AcademicRecordInfo;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sooa.domain.reg_and_auth_ms.User;
+
+import java.util.List;
 
 @Service
 public class AcademicRecordService {
@@ -14,9 +18,29 @@ public class AcademicRecordService {
         this.rest = restTemplate;
     }
 
-    public AcademicRecordInfo getAcademicRecord(String id){
+    public AcademicRecordInfo getAcademicRecord(String student_id){
         return rest.getForObject("http://localhost:4001/academic-record/{id}",
-                AcademicRecordInfo.class, id);
+                AcademicRecordInfo.class, student_id);
+    }
+
+    public List<AcademicRecordInfo> getAllAcademicrecords() {
+        return rest.getForObject("http://localhost:4001/academic-record",
+                List.class);
+    }
+
+    public AcademicRecordInfo createAcademicRecord(AcademicRecordInfo academicRecordInfo){
+        return rest.postForObject("http://localhost:4001/academic-record", academicRecordInfo,
+                AcademicRecordInfo.class);
+    }
+
+    public void updateAcademicRecord(String student_id) {
+        rest.put("http://localhost:4001/academic-record/{id}",
+                student_id);
+    }
+
+    public void deleteAcademicRecord(String student_id) {
+        rest.delete("http://localhost:4001/academic-record/{id}",
+                student_id);
     }
 
 }
