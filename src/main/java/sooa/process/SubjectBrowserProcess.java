46053f7884422023daa6367f1282b7a3a6209c64
Danny Garzón <degarzonm@unal.edu.co>
2022-10-28 12:15:38 -0500

    api gateway subjects

--- src/main/java/sooa/process/SubjectBrowserProcess.java
+++ src/main/java/sooa/process/SubjectBrowserProcess.java
@@ -1,25 +1,20 @@
 package sooa.process;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import sooa.domain.register_subjects_ms.*;
-import sooa.service.RMQMessagingService;
+import sooa.domain.subjects_ms.*;
 import sooa.service.SubjectBrowserService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class SubjectBrowserProcess {
     @Autowired
     private SubjectBrowserService sbsrvice;
-    
-    @Autowired
-    private RMQMessagingService rmqMessagingService;
-
     public Subject getSubjectById(String id) {
         return sbsrvice.getSubjectById(id);
     }
-    public List<Subject> getSubjectByName(String name) {
+    public Subject getSubjectByName(String name) {
         return sbsrvice.getSubjectByName(name);
     }
     
@@ -31,18 +26,18 @@ public class SubjectBrowserProcess {
         return sbsrvice.getCourseById(id);
     }
 
-    public List<Course> getCoursesBySubject(String sb){
-        return sbsrvice.getCoursesBySubject(sb);
+    public Course[] getCoursesBySubject(String subject){
+        return sbsrvice.getCoursesBySubject(subject);
     }
-    public List<Course> getCoursesByStudent(String st){
-        return sbsrvice.getCoursesByStudent(st);
+    public Course[] getCoursesByStudent(String student){
+        return sbsrvice.getCoursesByStudent(student);
     }
-    public List<Course> getCoursesBySubjectSem(String sb, int semester){
-        return sbsrvice.getCoursesBySubjectSem(sb,semester);
+    public Course[] getCoursesByStudentSem(String student, String semester){
+        return sbsrvice.getCoursesByStudentSem(student,semester);
     }
 
-    public List<Course> getCoursesByProfessor(String prof){
-        return sbsrvice.getCoursesByProfessor(prof);
+    public Course[] getCoursesByProfe(String profe){
+        return sbsrvice.getCoursesByProfe(profe);
     }
 
 }
