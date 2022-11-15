15bc35c17266eaca7a772be994a82bc5c4d7a960
Jimmy <jimmy.cardenas@mundi.io>
2022-11-14 21:52:30 -0500

    LDAP

--- src/main/java/sooa/ApiGatewayApplication.java
+++ src/main/java/sooa/ApiGatewayApplication.java
@@ -5,10 +5,12 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Import(WebSecurityConfig.class)
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
